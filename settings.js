3abaf57af21ea250393fc33e9e9731642e17673f
Ocenwave <Owave294@gmail.com>
2024-10-07 18:40:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348187637779"
-global.ownername = "ᴇꜱ ᴛᴇᴀᴍꜱ-ᴛᴇᴄʜ👑"
+global.ownernomer = "2347066939379"
+global.ownername = "EVIL ATTITUDE"
 global.ytname = "YT: paskito"
 global.socialm = "GitHub: paskito002"
 global.location = "Nigeria,Cross River,Calabar"
 
-global.ownernumber = '2348187637779'  //creator number
-global.ownername = 'ᴇꜱ ᴛᴇᴀᴍꜱ-ᴛᴇᴄʜ👑' //owner name
-global.botname = 'S҉ T҉ I҉ N҉ G҉ E҉ R҉- V҉ 2҉' //name of the bot
+global.ownernumber = '2347066939379'  //creator number
+global.ownername = 'EVIL ATTITUDE' //owner name
+global.botname = 'BLUE WAVE ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
